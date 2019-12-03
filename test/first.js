4e8ddf53f068195069d392feea7188b1e965be67
Mahesh Masalkar <mahi@Apples-MacBook-Pro.local>
2019-12-03 16:16:40 +0530

    Initialised git repo

---
+++ test/first.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+describe('RISHAB IS LEARNING TO TEST', function() {
+  describe('ARRAY #indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+
+  describe('ARRAY #indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
